423d713d09efecc923c847979a28cddc326c64b8
zhaizhipeng <zhaizhipeng@extracme.com>
2019-10-21 14:26:30 +0800

    rabbitmq 快速入门

--- src/main/java/com/raabbitmq/quickstart/Consumer.java
+++ src/main/java/com/raabbitmq/quickstart/Consumer.java
@@ -17,17 +17,19 @@ public class Consumer {
         String exchangeName = "test.quick.exchange";
         String queueName = "test_quick_queue";
         String routeKey = "test_quick_route";
-        Channel channel = connection.createChannel();
-        channel.exchangeDeclare(exchangeName,"direct",false,false,null);
-        channel.queueDeclare(queueName,true,true,false,null);
+        final Channel channel = connection.createChannel();
+        channel.exchangeDeclare(exchangeName,"direct",true,false,null);
+        channel.queueDeclare(queueName,true,false,false,null);
         channel.queueBind(queueName,exchangeName,routeKey);
 
-        channel.basicConsume(queueName, new DefaultConsumer(channel) {
+        channel.basicQos(1);
+        channel.basicConsume(queueName, false, new DefaultConsumer(channel) {
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 System.out.println("consumerTag:"+consumerTag);
                 System.out.println("envelope:"+envelope);
                 System.out.println("properties:"+properties);
                 System.out.println("body:"+new String(body));
+                channel.basicAck(envelope.getDeliveryTag(), false);
             }
         });
     }
