28ad664e32b369e461f0f14c1022ff2d4ffca8f8
zhaizhipeng <zhaizhipeng@extracme.com>
2019-10-22 13:47:15 +0800

    rabbitmq 快速入门

--- src/main/java/com/raabbitmq/quickstart/Consumer.java
+++ src/main/java/com/raabbitmq/quickstart/Consumer.java
@@ -6,30 +6,40 @@ import java.io.IOException;
 
 public class Consumer {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
+
+        //1.创建一个ConnectionFactory 并进行配置
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
         connectionFactory.setPort(5672);
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setHandshakeTimeout(20000);
+
+        //2.通过连接工厂创建连接
         Connection connection = connectionFactory.newConnection();
 
-        String exchangeName = "test.quick.exchange";
-        String queueName = "test_quick_queue";
-        String routeKey = "test_quick_route";
-        final Channel channel = connection.createChannel();
-        channel.exchangeDeclare(exchangeName,"direct",true,false,null);
-        channel.queueDeclare(queueName,true,false,false,null);
-        channel.queueBind(queueName,exchangeName,routeKey);
+        //3.通过Connection 创建一个 Channel
+        Channel channel = connection.createChannel();
 
-        channel.basicQos(1);
-        channel.basicConsume(queueName, false, new DefaultConsumer(channel) {
+        String queueName = "test";
+
+        /**
+         * durable 是否持久化
+         * exclusive 独占的相当于加了一把锁
+         */
+        //4.声明创建一个队列
+        channel.queueDeclare("test",true,false,false,null);
+
+        //6.设置channel
+        /**
+         * ACK: 当一条消息从生产端发到消费端，消费端接收到消息后会马上回送一个ACK信息给broker,告诉它这条消息收到了
+         * autoack:
+         *      true 自动签收 当消费者收到消息就表示消费者收到了消息，消费者收到了消息就会立即从队列中删除。
+         *      false 手动签收 当消费者收到消息在合适的时候来显示的进行确认，确认已经接收到了该消息了，RabbitMQ可以从队列中删除该消息了。
+         */
+        channel.basicConsume(queueName, true, new DefaultConsumer(channel) {
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                System.out.println("consumerTag:"+consumerTag);
-                System.out.println("envelope:"+envelope);
-                System.out.println("properties:"+properties);
-                System.out.println("body:"+new String(body));
-                channel.basicAck(envelope.getDeliveryTag(), false);
+                System.err.println("消费端:" + new String(body));
             }
         });
     }
--- src/main/java/com/raabbitmq/quickstart/Producer.java
+++ src/main/java/com/raabbitmq/quickstart/Producer.java
@@ -7,22 +7,33 @@ import com.rabbitmq.client.ConnectionFactory;
 public class Producer {
 
     public static void main(String[] args) throws Exception {
+
+        //1.创建一个ConnectionFactory并进行配置
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
         connectionFactory.setPort(5672);
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setHandshakeTimeout(20000);
+
+        //2.通过连接工厂创建连接
         Connection connection = connectionFactory.newConnection();
 
-        String exchangeName = "test.quick.exchange";
-        String queueName = "test_quick_queue";
-        String routeKey = "test_quick_route";
+        //3.通过Connection创建一个Channel
         Channel channel = connection.createChannel();
 
-        for (int i = 0; i < 100; i++){
-            String body = "hello rabbitmq" + i;
-            channel.basicPublish(exchangeName,routeKey,false,null,body.getBytes());
+        /**
+         * basicPublish(String exchange, String routingKey, BasicProperties props, byte[] body)
+         * exchange:指定交换机 不指定 则默认（AMQP default交换机） 通过routingkey进行匹配
+         * props 消息属性
+         * body 消息体
+         */
+        for(int i = 0; i < 5; i++){
+            String msg = "Hello RabbitMQ" + i;
+            //4.通过Channel发送数据
+            channel.basicPublish("", "test", null, msg.getBytes());
         }
+
+        //5.记得关闭相关的连接
         channel.close();
         connection.close();
     }
